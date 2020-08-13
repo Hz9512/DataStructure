00d2d218ebb2b7f21611d6b44b15b56825ed1693
Administrator <hzz9512@163.com>
2020-08-13 17:24:11 +0800

    代理

---
+++ src/com/daili/RentImpl.java
@@ -0,0 +1,8 @@
+package com.daili;
+
+public class RentImpl implements Rent{
+    @Override
+    public void rent() {
+        System.out.println("房东要发房源了");
+    }
+}
---
+++ src/com/daili/Test.java
@@ -0,0 +1,12 @@
+package com.daili;
+
+import java.lang.reflect.Proxy;
+
+public class Test {
+    public static void main(String[] args) {
+        Rent rent=new RentImpl();
+        Intermediary intermediary=new Intermediary(rent);
+        Rent rent1 = (Rent) Proxy.newProxyInstance(rent.getClass().getClassLoader(), rent.getClass().getInterfaces(), intermediary);
+        rent1.rent();
+    }
+}
